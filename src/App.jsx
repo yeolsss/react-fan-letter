ba1623dd4580820fb9763ed4dcbabbb164ffe739
yeolsss <yeol8810@gmail.com>
2023-11-15 21:31:29 +0900

    임시저장

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Router } from './shared/Router';
 import { StContainer } from './styles/Container';
 
 function App() {
-  // 용승, 진호, 명섭, 민석, 유나, 미래
   return (
     <StContainer>
       <Router />
